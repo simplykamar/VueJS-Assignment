8e266fab03847a9db7ec94f38b0b867071aef29d
kamar alam <53539093+simplykamar@users.noreply.github.com>
2024-07-19 11:45:19 +0530

    Assignment2

--- Assignment1/src/components/Test.vue
+++ Assignment1/src/components/Test.vue
@@ -1,9 +1,22 @@
+<script setup>
+    import {ref} from 'vue'
+    const name =  ref('')
+</script>
 <template>
-    <h1>Hello Deepak Sir.</h1>
+    <main>
+        <h1>Hello Vue My name is: {{ name }}</h1>
+        <input v-bind:value="name" v-on:input="event=>name=event.target.value"/>
+    </main>
 </template>
 
 <style scoped>
-    h1{
-        text-align: center;
+    main{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column
+    }
+    main > h1{
+        color: red
     }
 </style>
