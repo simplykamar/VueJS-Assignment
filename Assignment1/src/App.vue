8c941bf405a2b28ddeb69e19720f566a50dcdbd9
kamar alam <53539093+simplykamar@users.noreply.github.com>
2024-07-22 11:46:45 +0530

    assignment7

--- Assignment1/src/App.vue
+++ Assignment1/src/App.vue
@@ -1,7 +1,26 @@
 <script setup>
-    import Test from './components/Test.vue'
+    import Card from './components/Card.vue'
 </script>
 
 <template>
-    <Test/>
+    <Card>
+        <template v-slot:header>
+            <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="Avatar" style="width:50%">
+        </template>
+        <template v-slot:title>
+            <h2>Card title</h2>
+        </template>
+        <template v-slot:body>
+            <h4>Card body</h4>
+        </template>
+        <template v-slot:footer>
+            <p>Card footer</p>
+        </template>
+    
+    </Card>
+
 </template>
+
+<style scoper>
+
+</style>
